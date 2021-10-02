78bc5aa7c312df13b02f54f6d9522bdb9de68ac9
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-10-01 19:31:00 -0500

    Factura Suma

---
+++ factura_suma/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Ajusta de Factura Suma',
+    'version': '0.0',
+    'author': 'IT Reingenierias S de RL',
+    'depends': ['base', 'account', 'mail'],
+    'data': ['views/reporte.xml']
+}
